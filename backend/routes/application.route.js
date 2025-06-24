bc9707d5e4dedcc6bdc0cb7e652520b224847bea
kaushal320 <nepalkaushal69@gmail.com>
2025-06-24 14:15:07 +0545

    Backend part is done

---
+++ backend/routes/application.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import isAuthenticated from "../middleware/isAuthenticated.js";
+import {
+  applyJob,
+  getApplicants,
+  getAppliedJobs,
+  updateStatus,
+} from "../controllers/application.controller.js";
+const router = express.Router();
+
+router.route("/apply/:id").get(isAuthenticated, applyJob);
+router.route("/get").get(isAuthenticated, getAppliedJobs);
+router.route("/applicants/:id").get(isAuthenticated, getApplicants);
+router.route("/status/:id/update").post(isAuthenticated, updateStatus);
+export default router;
